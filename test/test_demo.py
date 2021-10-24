041422313eb1a0dec33699a4f58ebe08fb9fad3b
Lev <lev@gmail.com>
2021-10-24 14:12:56 +0300

    add test fixture

--- test/test_demo.py
+++ test/test_demo.py
@@ -3,13 +3,16 @@ import pytest
 from src.demo_app import Demo
 
 
-def test_something():
-    demo = Demo()
-    demo.set_val("val")
-    assert "val" == demo.get_val()
+@pytest.fixture
+def demo_fixture():
+    return Demo()
 
 
-def test_validation_raises_error():
-    demo = Demo()
+def test_something(demo_fixture):
+    demo_fixture.set_val("val")
+    assert "val" == demo_fixture.get_val()
+
+
+def test_validation_raises_error(demo_fixture):
     with pytest.raises(SystemError):
-        demo.validate()
+        demo_fixture.validate()
